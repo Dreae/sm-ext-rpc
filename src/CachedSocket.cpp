f8c04247fd6e395f1ff70d02b6b91b82fbc1d2db
Dreae <penitenttangentt@gmail.com>
2016-08-08 20:50:15 -0400

    Update cmakelist

--- src/CachedSocket.cpp
+++
@@ -1,9 +0,0 @@
-#include "CachedSocket.hpp"
-
-CachedSocket::CachedSocket(std::unique_ptr<boost::asio::ip::tcp::socket> socket) {
-  this->socket = std::move(socket);
-}
-
-bool CachedSocket::Connected() {
-
-}
