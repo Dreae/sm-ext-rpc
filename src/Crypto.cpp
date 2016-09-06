222d6d9ca75204c4a0f905a2ea7ab21468be85ee
Dreae <dreae@dreae.onl>
2016-09-05 20:27:12 -0400

    First pass at message signing

--- src/Crypto.cpp
+++ src/Crypto.cpp
@@ -2,7 +2,7 @@
 
 std::string Digest(std::string message, std::string key) {
   auto digest = HMAC(EVP_sha256(), key.c_str(), key.size(), reinterpret_cast<const unsigned char *>(message.c_str()), message.size(), NULL, NULL);
-  char hash[64];
+  char hash[128];
   for (int c = 0; c < 32; c++) {
     sprintf(&hash[c * 2], "%02x", digest[c]);
   }
